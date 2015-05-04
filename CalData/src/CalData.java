de8499c26a23a9ecd5ae958e46f511f7d649f6bf
geovani <geovani@geovani-N68S3B>
2015-05-04 00:10:03 -0300

    faz calculo de uma data futura

--- CalData/src/CalData.java
+++ CalData/src/CalData.java
@@ -2,6 +2,7 @@
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -27,15 +28,41 @@ public class CalData {
 		qtdDias = dt / 86400000L;
 		return qtdDias;
 	}
+	
+    /**calcula a quantidade de dias entre duas datas
+	 * data final - data inicial
+	 * @param datainicial
+	 * @param dataInicial
+	 * @return qtdDias
+	 */
+	@SuppressWarnings("deprecation")
+	private String calculaDataEntrega(String dataInicial) {
+		Date data = new Date(); 
+		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			data = formatador.parse(dataInicial);
+		} catch (ParseException e) {
+			System.err.println("Ocorreu um erro a gerar a data");
+		}
+		//data.setDate(data.getDate()+15);
+		Calendar c = Calendar.getInstance();
+		c.setTime(data);
+		c.add(Calendar.DATE, 15);
+		data = c.getTime();    
+		dataInicial = formatador.format(data);
+		return dataInicial;
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		CalData calculo= new CalData();
-		String datainicial = "10/04/2015";
+		String datainicial = "01/05/2015";
 		String dataFinal = calculo.formatador.format(calculo.data);
 		System.err.println(" data inicial: "+datainicial+"  data final: "+dataFinal);
 		System.out.println("qtd dias: "+calculo.calculaQtdDias(datainicial, dataFinal)+" dias");
+		
+		System.out.println("A data será: "+calculo.calculaDataEntrega(datainicial));
 	}
 
 }
